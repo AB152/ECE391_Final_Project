c66e4459ec101d0fa8819fe14b264c450b619e56
Franklin Moy <fmoy3@illinois.edu>
2021-03-20 23:59:38 -0500

    Create keyboard files

---
+++ student-distrib/keyboard.c
@@ -0,0 +1,14 @@
+#include "keyboard.h"
+#include "lib.h"
+#include "i8259.h"
+
+// http://www.philipstorr.id.au/pcbook/book3/scancode.htm
+
+void init_keyboard(){
+
+}
+
+
+void handle_keyboard(){
+
+}
