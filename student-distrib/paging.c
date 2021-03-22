78b36ba800b14f576772a880f1b0cbfe8e61a2a1
Xiao <gloriax2@illinois.edu>
2021-03-22 14:48:51 -0500

    added some comments

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -29,27 +29,23 @@ void init_paging() {
     int i;
 
     for(i=0;i < ONE_KB;i++){
-            //fill 8MB-4GB w/ blank pages b/c unused
-            //if(j!=0 && i>2){
-                page_directory[i]/*.one_gig[i]*/.pd_mb.present = 0; 
-                page_directory[i]/*.one_gig[i]*/.pd_mb.read_write = 1; //left as 1 b/c all pages are marked read/write for mp3
-                page_directory[i]/*.one_gig[i]*/.pd_mb.user_supervisor = 1;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_write_through = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_cache_disabled = 1;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.accessed = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.dirty = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_size = 1;   //left 1 to indicate directory is 4mb
-                page_directory[i]/*.one_gig[i]*/.pd_mb.global_bit = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.available = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_attr_index = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.reserved = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.base_addr = i;
+            //fill all of directory w/ blank pages b/c unused
+                page_directory[i].pd_mb.present = 0; 
+                page_directory[i].pd_mb.read_write = 1; //left as 1 b/c all pages are marked read/write for mp3
+                page_directory[i].pd_mb.user_supervisor = 1;
+                page_directory[i].pd_mb.page_write_through = 0;
+                page_directory[i].pd_mb.page_cache_disabled = 1;
+                page_directory[i].pd_mb.accessed = 0;
+                page_directory[i].pd_mb.dirty = 0;
+                page_directory[i].pd_mb.page_size = 1;   //left 1 to indicate directory is 4mb
+                page_directory[i].pd_mb.global_bit = 0;
+                page_directory[i].pd_mb.available = 0;
+                page_directory[i].pd_mb.page_attr_index = 0;
+                page_directory[i].pd_mb.reserved = 0;
+                page_directory[i].pd_mb.base_addr = i;
             //}
 
-        }      
-
-
-
+    }      
 
     // Blank/populate page table
     for(i=0; i<ONE_KB; i++){
@@ -78,68 +74,62 @@ void init_paging() {
     }
 
     // Initializes the memory belonging to the video memory (4 KB within the first 4 MB page)
-    page_directory[0]/*.one_gig[0]*/.pd_kb.present = 1;        //present b/c page is being initialized
-    page_directory[0]/*.one_gig[0]*/.pd_kb.read_write = 1;     //all pages are marked read/write for mp3
-    page_directory[0]/*.one_gig[0]*/.pd_kb.user_supervisor = 0;    //1 for user-level pages
-    page_directory[0]/*.one_gig[0]*/.pd_kb.page_write_through = 0; //we always want writeback, so 0
-    page_directory[0]/*.one_gig[0]*/.pd_kb.page_cache_disabled = 0; //0 for video memory pages
-    page_directory[0]/*.one_gig[0]*/.pd_kb.accessed = 0;   //not used at all in mp3
-    page_directory[0]/*.one_gig[0]*/.pd_kb.reserved = 0;   //all reserved bits should be set to 0
-    page_directory[0]/*.one_gig[0]*/.pd_kb.page_size = 0;  //0 if 4K page directory entry
-    page_directory[0]/*.one_gig[0]*/.pd_kb.global_bit = 0; //0 b/c not kernel page
-    page_directory[0]/*.one_gig[0]*/.pd_kb.available = 0;  //not used at all in mp3
-    page_directory[0]/*.one_gig[0]*/.pd_kb.page_table_addr = (unsigned)page_table_one >> 12;   //shift address of table so that lower 12 bits are removed
+    page_directory[0].pd_kb.present = 1;        //present b/c page is being initialized
+    page_directory[0].pd_kb.read_write = 1;     //all pages are marked read/write for mp3
+    page_directory[0].pd_kb.user_supervisor = 0;    //1 for user-level pages
+    page_directory[0].pd_kb.page_write_through = 0; //we always want writeback, so 0
+    page_directory[0].pd_kb.page_cache_disabled = 0; //0 for video memory pages
+    page_directory[0].pd_kb.accessed = 0;   //not used at all in mp3
+    page_directory[0].pd_kb.reserved = 0;   //all reserved bits should be set to 0
+    page_directory[0].pd_kb.page_size = 0;  //0 if 4K page directory entry
+    page_directory[0].pd_kb.global_bit = 0; //0 b/c not kernel page
+    page_directory[0].pd_kb.available = 0;  //not used at all in mp3
+    page_directory[0].pd_kb.page_table_addr = (unsigned)page_table_one >> 12;   //shift address of table so that lower 12 bits are removed
 
     // Initializes the memory belonging to the kernel (4 MB - 8 MB)
-    page_directory[1]/*.one_gig[1]*/.pd_mb.present = 1;    //present b/c page is being initialized
-    page_directory[1]/*.one_gig[1]*/.pd_mb.read_write = 1;     //all pages are marked read/write for mp3
-    page_directory[1]/*.one_gig[1]*/.pd_mb.user_supervisor = 0;    //0 for kernel pages
-    page_directory[1]/*.one_gig[1]*/.pd_mb.page_write_through = 0;    //we always want writeback, so 0
-    page_directory[1]/*.one_gig[1]*/.pd_mb.page_cache_disabled = 1;    //1 for program code and data pages (kernel pages)
-    page_directory[1]/*.one_gig[1]*/.pd_mb.accessed = 0;   //not used at all in mp3
-    page_directory[1]/*.one_gig[1]*/.pd_mb.dirty = 0;      //not used at all in mp3
-    page_directory[1]/*.one_gig[1]*/.pd_mb.page_size = 1;  //1 if 4M page directory entry
-    page_directory[1]/*.one_gig[1]*/.pd_mb.global_bit = 1; // the kernel is mapped into every process’s address space
-    page_directory[1]/*.one_gig[1]*/.pd_mb.available = 0;  //not used at all in mp3
-    page_directory[1]/*.one_gig[1]*/.pd_mb.page_attr_index = 0;  //not used at all in mp3
-    page_directory[1]/*.one_gig[1]*/.pd_mb.reserved = 0;       //reserved bits are always set to 0
-    page_directory[1]/*.one_gig[1]*/.pd_mb.base_addr = 1;      // Kernel is at first 4MB
+    page_directory[1].pd_mb.present = 1;    //present b/c page is being initialized
+    page_directory[1].pd_mb.read_write = 1;     //all pages are marked read/write for mp3
+    page_directory[1].pd_mb.user_supervisor = 0;    //0 for kernel pages
+    page_directory[1].pd_mb.page_write_through = 0;    //we always want writeback, so 0
+    page_directory[1].pd_mb.page_cache_disabled = 1;    //1 for program code and data pages (kernel pages)
+    page_directory[1].pd_mb.accessed = 0;   //not used at all in mp3
+    page_directory[1].pd_mb.dirty = 0;      //not used at all in mp3
+    page_directory[1].pd_mb.page_size = 1;  //1 if 4M page directory entry
+    page_directory[1].pd_mb.global_bit = 1; // the kernel is mapped into every process’s address space
+    page_directory[1].pd_mb.available = 0;  //not used at all in mp3
+    page_directory[1].pd_mb.page_attr_index = 0;  //not used at all in mp3
+    page_directory[1].pd_mb.reserved = 0;       //reserved bits are always set to 0
+    page_directory[1].pd_mb.base_addr = 1;      // Kernel is at first 4MB
 
-    //for(j=0;j<4;j++){ 
-        for(i=2;i<ONE_KB;i++){
-            //fill 8MB-4GB w/ blank pages b/c unused
-            //if(j!=0 && i>2){
-                page_directory[i]/*.one_gig[i]*/.pd_mb.present = 0; 
-                page_directory[i]/*.one_gig[i]*/.pd_mb.read_write = 1; //left as 1 b/c all pages are marked read/write for mp3
-                page_directory[i]/*.one_gig[i]*/.pd_mb.user_supervisor = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_write_through = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_cache_disabled = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.accessed = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.dirty = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_size = 1;   //left 1 to indicate directory is 4mb
-                page_directory[i]/*.one_gig[i]*/.pd_mb.global_bit = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.available = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.page_attr_index = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.reserved = 0;
-                page_directory[i]/*.one_gig[i]*/.pd_mb.base_addr = i;
-            //}
+    for(i=2;i<ONE_KB;i++){
+            page_directory[i].pd_mb.present = 0; 
+            page_directory[i].pd_mb.read_write = 1; //left as 1 b/c all pages are marked read/write for mp3
+            page_directory[i].pd_mb.user_supervisor = 0;
+            page_directory[i].pd_mb.page_write_through = 0;
+            page_directory[i].pd_mb.page_cache_disabled = 0;
+            page_directory[i].pd_mb.accessed = 0;
+            page_directory[i].pd_mb.dirty = 0;
+            page_directory[i].pd_mb.page_size = 1;   //left 1 to indicate directory is 4mb
+            page_directory[i].pd_mb.global_bit = 0;
+            page_directory[i].pd_mb.available = 0;
+            page_directory[i].pd_mb.page_attr_index = 0;
+            page_directory[i].pd_mb.reserved = 0;
+            page_directory[i].pd_mb.base_addr = i;
 
-        }                     
-    //}
-    
+    }       
+
     flush_tlb();
 
-    // Load page directory address in register CR3
-    asm volatile ("movl  %0, %%eax;"
-                  //"andl $0xFFFFFFE7, %%eax;"
+    // Load and enable page directory address in register CR3
+    asm volatile ("movl  %0, %%eax;"                //loads page directory
                   "movl %%eax, %%cr3;"
-                  "movl %%cr4, %%eax;"
-                  "orl $0x00000010, %%eax;"
+                  "movl %%cr4, %%eax;"              
+                  "orl $0x00000010, %%eax;"         
                   "movl %%eax, %%cr4;"
-                  "movl %%cr0, %%eax;"
+                  "movl %%cr0, %%eax;"              //enables page directory
                   "orl $0x80000000, %%eax;"
                   "movl %%eax, %%cr0;"
-                :               // no outputs
+                :                                   // no outputs
                 : "r" (page_directory)              // no inputs
                 : "eax", "cc"        // clobbers eax and condition codes
     );
