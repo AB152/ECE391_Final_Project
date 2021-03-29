25f93162c233b251ad8f2e90fb511a8c9f3a8ba3
Xiao <gloriax2@illinois.edu>
2021-03-28 19:18:13 -0500

    finished all fs functions, still need to test

--- student-distrib/file_system.c
+++ student-distrib/file_system.c
@@ -4,13 +4,12 @@
 
 void init_filesystem(uint32_t start){
     boot=(boot_block_t*)start;      //points to starting memory block of file system 
-    inode=(inode_t*)(start+BLOCK_SIZE);   //inodes start one block (4KB) after start/boot
-    dir_entry=(dentry_t*)(start+64);   //dir entries start 64B after start/boot 
-    data_block=(data_block_t*)(start+BLOCK_SIZE*(boot->num_inodes+1)); //data block starts a block after inode
+    fs_inode=(inode_t*)(start+BLOCK_SIZE);   //inodes start one block (4KB) after start/boot
+    fs_dentry=(dentry_t*)(start+64);   //dir entries start 64B after start/boot 
+    fs_data_block=(data_block_t*)(start+BLOCK_SIZE*(boot->num_inodes+1)); //data block starts a block after inode
 }
 
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
-    int i=0;
     if(fname==NULL||dentry==NULL)   //check for invalid pointers
         return -1;
 
@@ -18,6 +17,7 @@ int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
     if(name_length>FNAME_LENGTH)      //check if name length goes beyond limit
         return -1;
     
+    int i;
     for(i=0;i<boot->num_dentries;i++){      //loop through all dentries
         /*if names match, copy over dentry file name, type, and index node into dentry block*/
         if(strncmp(&(boot->dentries[i]),fname,FNAME_LENGTH)){     
@@ -46,11 +46,32 @@ int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry){
 }
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
+    if(buf==NULL)           //check for invalid pointer
+        return -1;
 
+    if(inode >= boot->num_inodes)      //check if index node is out of bounds
+        return -1;
+    
+    inode_t* curr_inode=(inode_t*)(&(fs_inode[inode])); //points to the file with inode number inode
+
+    if(offset >= curr_inode->file_size) //check if offset from start of file is out of bounds
+        return -1;
+
+    int bytes_read;
+    data_block_t* curr_data_block;
+
+    for(bytes_read=0; bytes_read<length; bytes_read++){     //loop through bytes that need to be read
+        if((offset+bytes_read) > curr_inode->file_size)     //break if bytes read go out of file bounds
+            break;
+        curr_data_block=(data_block_t*)(fs_data_block + (curr_inode->index_num[offset/BLOCK_SIZE])); //finds start of data block of which to read bytes from
+        buf[bytes_read]=curr_data_block->block[offset%BLOCK_SIZE];  //copy data into buf
+        offset++;
+    }
+    return bytes_read;
 }
 
-int32_t read_file(int32_t fd, void* buf, int32_t nbytes){
-
+int32_t read_file(uint32_t inode, uint32_t offset, void* buf, int32_t nbytes){
+    return read_data(inode, offset, (uint8_t*)buf, nbytes);
 }
 
 int32_t write_file(int32_t fd, const void* buf, int32_t nbytes){
@@ -65,8 +86,18 @@ int32_t close_file (int32_t fd){
     return 0;           //closing is always successful
 }
 
-int32_t read_dir(int32_t fd, void* buf, int32_t nbytes){
-
+int32_t read_dir(uint32_t inode, uint32_t offset, uint8_t* buf, int32_t nbytes){
+    if(buf==NULL)       //check for null pointer
+        return -1;
+    
+    int bytes_read;
+    for(bytes_read=0; bytes_read<nbytes; bytes_read++){     //loop through bytes that need to be read
+        if((offset/FNAME_LENGTH) > boot->num_dentries)        //check if curr directory goes out of bounds
+            break;
+        buf[bytes_read]=boot->dentries[offset/FNAME_LENGTH].fname[offset%FNAME_LENGTH]; //copy over file name into buf
+        offset++;
+    }
+    return bytes_read;
 }
 
 int32_t write_dir(int32_t fd, const void* buf, int32_t nbytes){
--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -11,7 +11,7 @@ typedef struct{
 }data_block_t;
 
 typedef struct{
-    uint32_t index_num[1023];   //holds indices for 1KB of a data block
+    uint32_t index_num[1023];   //holds max data block index (1KB)
     uint32_t file_size;         //described in bytes, used for regular files
 }inode_t;
 
@@ -32,10 +32,10 @@ typedef struct{
 }dentry_t;
 
 /*global variables that will keep track of entire file system structure*/
-data_block_t* data_block;
-inode_t* inode;
+data_block_t* fs_data_block;
+inode_t* fs_inode;
 boot_block_t* boot;       
-dentry_t* dir_entry;       
+dentry_t* fs_dentry;       
 
 //initializes filesystem based off start pointer
 extern void init_filesystem(uint32_t start);
@@ -46,12 +46,14 @@ extern int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
 extern int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 
 /*required system call functions from Appendix B for files and directories*/
-extern int32_t read_file(int32_t fd, void* buf, int32_t nbytes);
+// extern int32_t read_file(int32_t fd, void* buf, int32_t nbytes);         //format cant be used for cp2 bc of fd
+extern int32_t read_file(uint32_t inode, uint32_t offset, uint8_t* buf, int32_t nbytes);
 extern int32_t write_file(int32_t fd, const void* buf, int32_t nbytes);
 extern int32_t open_file(const uint8_t* filename);
 extern int32_t close_file (int32_t fd);
 
-extern int32_t read_dir(int32_t fd, void* buf, int32_t nbytes);
+// extern int32_t read_dir(int32_t fd, void* buf, int32_t nbytes);          //format cant be used for cp2 bc of fd
+extern int32_t read_dir(uint32_t inode, uint32_t offset, uint8_t* buf, int32_t nbytes);
 extern int32_t write_dir(int32_t fd, const void* buf, int32_t nbytes);
 extern int32_t open_dir(const uint8_t* dirname);
 extern int32_t close_dir (int32_t fd);
